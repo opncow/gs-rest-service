5bf533dd527085ce6d58e4ff37942798c7d56ebc
Alex <opncow@googlemail.com>
2017-05-12 01:31:49 +0200

    Minimal Spring-Boot w/ Shiro example.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,12 +1,82 @@
 package hello;
 
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.AbstractShiroFilter;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+
+import javax.servlet.DispatcherType;
+import java.util.EnumSet;
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(name = "shiroFilter")
+    public FilterRegistrationBean shiroFilter() throws Exception {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter((AbstractShiroFilter) getShiroFilterFactoryBean().getObject());
+        registration.setDispatcherTypes(EnumSet.allOf(DispatcherType.class));
+        return registration;
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean getShiroFilterFactoryBean() {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager());
+
+        Map<String, String> filterChainDefinitionMap = shiroFilterFactoryBean.getFilterChainDefinitionMap();
+        filterChainDefinitionMap.put("/**", "authcBasic");
+
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager securityManager() {
+        DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
+        dwsm.setRealm(getShiroIniRealm());
+        final DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        // disable session cookie
+        sessionManager.setSessionIdCookieEnabled(false);
+        dwsm.setSessionManager(sessionManager);
+        return dwsm;
+    }
+
+    @Bean(name = "shiroIniRealm")
+    @DependsOn("lifecycleBeanPostProcessor")
+    public IniRealm getShiroIniRealm() {
+        return new IniRealm("classpath:shiro.ini");
+    }
+
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator daap = new DefaultAdvisorAutoProxyCreator();
+        daap.setProxyTargetClass(true);
+        return daap;
+    }
+
+    @Bean
+    public AuthorizationAttributeSourceAdvisor getAuthorizationAttributeSourceAdvisor() {
+        AuthorizationAttributeSourceAdvisor aasa = new AuthorizationAttributeSourceAdvisor();
+        aasa.setSecurityManager(securityManager());
+        return new AuthorizationAttributeSourceAdvisor();
     }
 }
